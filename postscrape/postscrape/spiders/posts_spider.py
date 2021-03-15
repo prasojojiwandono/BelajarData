0325c751b5e26bcce6987bc2844e51103417f411
prasojo <jiwandonoprasojo@gmail.com>
2021-03-16 05:33:31 +0700

    improve logic

--- postscrape/postscrape/spiders/posts_spider.py
+++ postscrape/postscrape/spiders/posts_spider.py
@@ -27,10 +27,12 @@ class PostsSpider(scrapy.Spider):
     wb_analyst = Workbook()
     sheet_analyst = ''
     pd_data = []
+    list_jii70 = []
 
     def __init__(self):
         print('init dimulai')
         self.sebelum = datetime.now()
+        self.delete_files()
         self.isi_urls()
         self.excel_initialization()
 
@@ -51,14 +53,11 @@ class PostsSpider(scrapy.Spider):
 
         if self.itung == len(self.start_urls):
             self.wb_analyst.save('/home/prasojo/belajar/BelajarData/dataset/saham/JII_analysis.xlsx') 
-            self.sesudah = datetime.now()
-            selisih = self.sesudah - self.sebelum
-            print(selisih)
-            sleep(3)
             from matplotlib.pyplot import savefig
             import mplfinance as mpf
             directory = '/home/prasojo/belajar/BelajarData/dataset/saham/emiten'
             pic_directory = '/home/prasojo/belajar/BelajarData/dataset/saham/emiten_pic'
+            pic_directory_up = '/home/prasojo/belajar/BelajarData/dataset/saham/emiten_pic_up'
             count = 1
             stats_data = []
             for filename in os.listdir(directory):
@@ -66,24 +65,38 @@ class PostsSpider(scrapy.Spider):
                 df_saham = pd.read_excel(file_name)
                 stat_data = self.stat_process(df_saham)
                 stats_data.append(stat_data)
-                df_saham = df_saham.reindex(index=df_saham.index[::-1])
-                df_saham = df_saham.set_index(pd.DatetimeIndex(df_saham['date'].values))
-                emiten = filename.split('.')[0]
-                print(emiten)
-                pic_file_name = f'{pic_directory}/{emiten}.jpg'
-                save = dict(fname=pic_file_name,dpi=100)
-                mpf.plot(df_saham,type='candle',mav=(3,7),volume=True,figsize=(25,15),savefig=save)
-                count += 1
-            df_stat = pd.DataFrame(stats_data,columns=['emiten', 'macd', 'rsi','percent_volum'])
+                if stat_data[2] == 'up_near':
+                    df_saham = df_saham.reindex(index=df_saham.index[::-1])
+                    df_saham = df_saham.set_index(pd.DatetimeIndex(df_saham['date'].values))
+                    emiten = filename.split('.')[0]
+                    print(emiten, count)
+                    pic_file_name = f'{pic_directory_up}/{emiten}.jpg'
+                    save = dict(fname=pic_file_name,dpi=100)
+                    mpf.plot(df_saham,type='candle',mav=(3,7),volume=True,figsize=(25,15),savefig=save)
+                    count += 1                    
+                elif df_saham['JII'][0] in self.list_jii70:
+                    df_saham = df_saham.reindex(index=df_saham.index[::-1])
+                    df_saham = df_saham.set_index(pd.DatetimeIndex(df_saham['date'].values))
+                    emiten = filename.split('.')[0]
+                    print(emiten, count)
+                    pic_file_name = f'{pic_directory}/{emiten}.jpg'
+                    save = dict(fname=pic_file_name,dpi=100)
+                    mpf.plot(df_saham,type='candle',mav=(3,7),volume=True,figsize=(25,15),savefig=save)
+                    count += 1
+            df_stat = pd.DataFrame(stats_data,columns=['emiten', 'macd', 'up_near_macd', 'rsi','percent_volum'])
             df_stat.to_excel(f'/home/prasojo/belajar/BelajarData/dataset/saham/analisa_saham_harian_v3.xlsx')
+            self.sesudah = datetime.now()
+            selisih = self.sesudah - self.sebelum
+            print(selisih)
 
 
     def stat_process(self, df):
         emiten = df['JII'][0]
         macd = self.calculate_macd(df['Close'])
+        up_near_macd = self.calculate_up_near_macd(df['Close'])
         rsi = self.calculate_rsi(df['Close'])
         percent_volum = self.calculate_percent_volum(df['Volume'])
-        return [emiten,macd,rsi,percent_volum]
+        return [emiten,macd,up_near_macd,rsi,percent_volum]
 
     def calculate_macd(self, df_close, ma_small=3, ma_big=7):
         current_ma_small = mean(df_close[:ma_small])
@@ -105,6 +118,27 @@ class PostsSpider(scrapy.Spider):
             return 'down'
         else:
             return '---'
+
+    def calculate_up_near_macd(self, df_close, near_degree=0.5, ma_small=3, ma_big=7):
+        current_ma_small = mean(df_close[:ma_small])
+        previous_ma_small = mean(df_close[1:ma_small+1])
+        current_ma_big = mean(df_close[:ma_big])
+        previous_ma_big = mean(df_close[1:ma_big+1])
+
+        con_a = current_ma_big > current_ma_small
+        con_b = current_ma_small > previous_ma_small
+        con_c = (current_ma_big-current_ma_small) < (previous_ma_big-previous_ma_small)
+        con_d = previous_ma_big > previous_ma_small
+        con_e = current_ma_big <= previous_ma_big
+
+        if con_a and con_b and con_c and con_d and con_e:
+            x = previous_ma_big - previous_ma_small - current_ma_big + current_ma_small
+            if x != 0:
+                distance = (current_ma_big-current_ma_small)/x
+                if distance >=0 and distance <= near_degree:
+                    return 'up_near'
+        return '---'
+        
 
     def calculate_percent_volum(self, df_volume, coverage=7):
         current_volume = df_volume[0]
@@ -220,9 +254,15 @@ class PostsSpider(scrapy.Spider):
     def isi_urls(self):
         loc = ("/home/prasojo/belajar/BelajarData/dataset/saham/JII.xlsx")
         wb_jii_list = xlrd.open_workbook(loc)
-        sheet_jii_list = wb_jii_list.sheet_by_index(0)
-        for i in range(sheet_jii_list.nrows):
-            emiten = sheet_jii_list.cell_value(i, 0)
+
+        sheet_jii_70 = wb_jii_list.sheet_by_index(0)
+        for x in range(sheet_jii_70.nrows):
+            emiten = sheet_jii_70.cell_value(x, 0)
+            self.list_jii70.append(emiten)
+
+        sheet_jii_large = wb_jii_list.sheet_by_index(1)
+        for i in range(sheet_jii_large.nrows):
+            emiten = sheet_jii_large.cell_value(i, 0)
             self.start_urls.append('https://finance.yahoo.com/quote/'+emiten+'.JK/history?p='+emiten+'.JK')
 
     def excel_initialization(self):
@@ -235,8 +275,15 @@ class PostsSpider(scrapy.Spider):
         self.sheet_analyst.write(0, 5, 'Close') 
         self.sheet_analyst.write(0, 6, 'Volume')
 
-        
-
-
-        
-            
+    def delete_files(self):
+        list_dir=[
+            '/home/prasojo/belajar/BelajarData/dataset/saham/emiten',
+            '/home/prasojo/belajar/BelajarData/dataset/saham/emiten_pic',
+            '/home/prasojo/belajar/BelajarData/dataset/saham/emiten_pic_up',
+        ]
+        for dir in list_dir:
+            if len(os.listdir(dir)) > 0:
+                for filename in os.listdir(dir):
+                    file_name = f'{dir}/{filename}'
+                    if os.path.isfile(file_name):
+                        os.remove(file_name)
